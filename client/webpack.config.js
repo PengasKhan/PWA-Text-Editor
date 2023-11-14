2fc815bd3c3b145f356a8b7ae6c19f016d2d29a1
PengasKhan <cheesepuff42@gmail.com>
2023-11-13 20:15:01 -0600

    renamed config
    names match project

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "Just Another Text Editor",
       }),
 
       // Injects our custom service worker
@@ -34,8 +34,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E.",
         description: "Never forget your contacts!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
